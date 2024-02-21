899886171f5a6c3f31f027cb3e4de673d259866d
Isaac Hamblin <isaac.hamblin@gmail.com>
2024-02-21 09:38:37 -0700

    added functions addListItem, updateHTMLList, event Listeners

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 console.log('My code is running');
 
-let TDLArr = []
+let TDLArr = [];
+let list = document.querySelector("#TDL");
+let addItemButton = document.querySelector("#addItemButton");
+let listInputBox = document.querySelector("#listInputBox");
+let listInputString = "";
 
-const =
+const addListItem = (item) => {
+    TDLArr.push(listInputString);
+    console.log(TDLArr);
+    updateHTMLList();
+};
+
+// function that updates our html list
+const updateHTMLList () => {
+    list.innerHTML = "";
+    for (let item of TDLArr) {
+        list.appendChild("<li>TEST</li>");
+    }
+};
+
+listInputBox.addEventListener("input", (event) => {
+    listInputString = event.target.value;
+});
+
+addItemButton.addEventListener("click", addListItem);
